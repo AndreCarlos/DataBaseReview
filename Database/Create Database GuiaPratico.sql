7236a7a390ae03dffff7267a6928f5a6ddad86c1
André Carlos <AndreCarlos@users.noreply.github.com>
2017-07-22 17:18:52 -0300

    Correction
    A little English mistake...

--- Database/Create Database GuiaPratico.sql
+++ Database/Create Database GuiaPratico.sql
@@ -4,7 +4,7 @@ CREATED BY : ANDRE CARLOS
 DATE OF CREATION : JUL/2017
 SUMMARY:
 This script is for a personal review of database programming and administration.
-This can by used by anyone who want study database technology. Future scripts will
+This can by used by anyone who wants study database technology. Future scripts will
 include PL/SQL and NoSQL programming.
 
 contato@andrecarlos.com.br
